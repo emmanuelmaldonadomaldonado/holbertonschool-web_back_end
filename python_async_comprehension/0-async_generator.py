c9f8dad803a0c982b0b74c4513a2ed29bc19b9e3
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-04-24 14:05:58 -0400

    .

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ Async Generator """
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """
         Generate numbers
 
