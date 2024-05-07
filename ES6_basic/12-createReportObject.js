233d2527adea66093ba9e9ab0db2ac941d9e644b
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-05-07 13:21:13 -0400

    .

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,14 +1,10 @@
 export default function createReportObject(employeesList) {
-    const allEmployees = {};
-  
-    for (const [departmentName, employees] of Object.entries(employeesList)) {
-      allEmployees[departmentName] = [...employees];
-    }
-  
-    return {
-      allEmployees: allEmployees,
-      getNumberOfDepartments() {
-        return Object.keys(allEmployees).length;
-      },
-    };
-  }
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
