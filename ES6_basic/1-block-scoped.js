2bfd2bef432617b152774a13f16fa634d3d1b599
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-05-06 22:42:43 -0400

    .

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-	const task = false;
-	const task2 = true;
+    if (trueOrFalse) {
+        var task = true;
+        var task2 = false;
+      }
 
-	if (trueOrFalse) {
-		/*eslint-disable */
-		const task = true;
-		const task2 = false;
-	}
+    var task = false;
+    var task2 = true;
 
-	return [task, task2];
-}
+    return [task, task2];
+  }
