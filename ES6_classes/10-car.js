736593f64ac97c28e730aebd7b2357fc1f1154a7
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-05-07 22:35:18 -0400

    .

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,16 +1,16 @@
-const _carId = Symbol('carId');
-
 export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
-    this[_carId] = Math.random().toString(36).substring(7);
+  }
+
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    const clone = new Car(this._brand, this._motor, this._color);
-    clone[_carId] = this[_carId];
-    return clone;
+    const ModelCar = this.constructor[Symbol.species];
+    return new ModelCar();
   }
 }
