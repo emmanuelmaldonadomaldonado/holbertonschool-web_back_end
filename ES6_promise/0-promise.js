0434f98392a78f48f3f2448bf13347725881766d
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-05-14 18:27:04 -0400

    .

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,13 +1,12 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // Aquí puedes realizar cualquier operación asíncrona, como una solicitud a una API.
-      // Por ahora, simplemente resolveremos la promesa después de un pequeño retraso.
-  
-      setTimeout(() => {
-        resolve("Response from API");
-      }, 1000); // Simulamos un retraso de 1 segundo
-    });
-  }
-  
-  export default getResponseFromAPI;
-  
+  return new Promise((resolve) => {
+    // Aquí puedes realizar cualquier operación asíncrona, como una solicitud a una API.
+    // Por ahora, simplemente resolveremos la promesa después de un pequeño retraso.
+
+    setTimeout(() => {
+      resolve('Response from API');
+    }, 1000); // Simulamos un retraso de 1 segundo
+  });
+}
+
+export default getResponseFromAPI;
