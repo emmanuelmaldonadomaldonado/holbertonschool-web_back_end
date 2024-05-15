edc3d54c33847af2518947cc2692c96497b4eb9b
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-05-15 10:35:29 -0400

    .

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,22 +1,9 @@
+// Handle multiple promises
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  try {
-    const [userData, photoData] = await Promise.all([userPromise, photoPromise]);
-
-    return [
-      { status: 'fulfilled', value: userData },
-      { status: 'fulfilled', value: photoData },
-    ];
-  } catch (error) {
-    return [
-      { status: 'rejected', value: error },
-    ];
-  }
+function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 }
 
 export default handleProfileSignup;
