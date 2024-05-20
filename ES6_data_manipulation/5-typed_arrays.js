5e6ec0b30fa52c136d2ab27aba6763cc410e5087
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-05-20 16:58:29 -0400

    .

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,14 @@
 function createInt8TypedArray(length, position, value) {
-    const buffer = new ArrayBuffer(length);
-    const dataView = new DataView(buffer);
-    
-    if (position >= length || position < 0) {
-      throw new Error('Position outside range');
-    }
-    
-    dataView.setInt8(position, value);
-    
-    return dataView;
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
+
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
   }
-  
-  export default createInt8TypedArray;
+
+  dataView.setInt8(position, value);
+
+  return dataView;
+}
+
+export default createInt8TypedArray;
